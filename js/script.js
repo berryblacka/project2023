92f9d0b67dc150be37781d581141dd6c79d2035b
yar1kukharenko <116860258+berryblacka@users.noreply.github.com>
2023-07-19 12:36:59 +0500

    wefw

--- js/script.js
+++ js/script.js
@@ -17,24 +17,13 @@ function animatePulse() {
 }
 
 // Функция для плавного появления нового элемента
-function fadeInElement() {
-  anime({
-    targets: '#fadeInElement',
-    begin() {
-      document.getElementById('fadeInElement').style.display = 'block';
-    },
-    opacity: (0, 1), // Целевое значение непрозрачности (1 - полностью видимый)
-    duration: 2000, // Продолжительность анимации в миллисекундах
-    easing: 'easeInOutSine', // Эффект плавности анимации
-  });
-}
 
 // Запускаем анимацию пульсации при загрузке страницы
 $(document).ready(() => {
   animatePulse();
   $('#pulseElement').on('click', () => {
     // Вызываем функцию плавного появления нового элемента
-    fadeInElement();
+    $('#fadeInElement').toggleClass('dis');
   });
 });
 
